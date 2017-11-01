cebce4ac0096e7122e7453dc197f1f1a92f518ac
Unknown <romiber@gmail.com>
2017-11-01 17:52:42 -0300

    Superposición v2.0 :)

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=AQUASOFT',
-    'username' => 'lia',
-    'password' => 'emilio1892',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
