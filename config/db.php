d75f39b6776673b42f12ab62d1aa7c3367d330d8
Unknown <liamoreno14@gmail.com>
2017-11-04 15:21:37 -0300

    Botones agregados - falta modal

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=AQUASOFT_FINAL',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=AQUASOFT',
+    'username' => 'lia',
+    'password' => 'emilio1892',
     'charset' => 'utf8',
 ];
--- views/supply/index.php
+++ views/supply/index.php
@@ -3,11 +3,12 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use yii\helpers\Url;
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\supply\SupplySearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Supplies';
+$this->title = 'Insumos';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="supply-index">
@@ -16,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Supply', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Crear insumo', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 <?php Pjax::begin(); ?>    <?= GridView::widget([
         'dataProvider' => $dataProvider,
@@ -31,7 +32,40 @@ $this->params['breadcrumbs'][] = $this->title;
             'activo',
             // 'TIPO_TAREA_idTipoTarea',
 
-            ['class' => 'yii\grid\ActionColumn'],
+            ['class' => 'yii\grid\ActionColumn',
+
+            'template'=>'{view}{update}{delete}',
+            'buttons' => [
+                'view'=>function($url,$model){
+                    return Html::button('<span class="btn-aquarium glyphicon glyphicon-eye-open"></span>',
+                    [
+                      'value' => Url::to(['supply/view']),
+                      'title' => 'Información del especialista: ',
+                      'class' => 'showModalButton btn btn-warning btnAquarium'
+                    ]);
+                },
+                'update'=>function($url,$model){
+                    return Html::button('<span class="btn-aquarium glyphicon glyphicon-pencil"></span>',
+                    [
+                      'value' => Url::to(['supply/update']),
+                      'title' => 'Modificar especialista: ',
+                      'class' => 'showModalButton btn btn-primary btnAquarium'
+                    ]);
+                },
+                'delete' => function($url, $model){
+                        return Html::a('<span class="glyphicon glyphicon-trash"></span>', ['supply'], [
+                            'class' => 'btn btn-success btnAquarium',
+                            'data' => [
+                                'data-pjax' => '0',
+                                'confirm' => '¿Está seguro de querer dar de alta el usuario"?',
+                                'method' => 'post',
+                            ],
+                        ]);
+
+                }
+            ]
+
+          ],
         ],
     ]); ?>
 <?php Pjax::end(); ?></div>
