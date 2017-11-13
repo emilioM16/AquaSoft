1ee8d333fdb743475fe9d3359d301d9aa7cf66e9
Emilio Melo <emiliops11@gmail.com>
2017-11-13 18:34:58 -0300

    Una pavada

--- views/aquarium/create.php
+++ views/aquarium/create.php
@@ -1,11 +1,11 @@
 <?php
 
 use yii\helpers\Html;
-
+use kartik\switchinput\SwitchInputAsset;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Aquarium */
-
+SwitchInputAsset::register($this); 
 $this->title = 'Agregar acuario';
 $this->params['breadcrumbs'][] = ['label' => 'Acuarios', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
